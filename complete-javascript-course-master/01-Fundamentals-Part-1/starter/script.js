22f16cf1436e66ec1cda99d0993d3b2994c85856
ZMAN <etta1055@gmail.com>
2021-05-12 12:02:04 -0500

    added files

---
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,129 @@
+// const age = 15;
+
+
+// if (age >= 18) {
+//     console.log('Sarah can start driving license 🤣');
+// } else {
+//     const yearsLeft = 18 - age;
+//     console.log(`Sarah is to young. wait another ${yearsLeft} years :)`);
+// }
+
+// const birthYear = 1998;
+// let centuary
+// if (birthYear <= 2000) {
+//     let century = 20;
+// } else {
+//     let century = 21;
+// 
+
+// const massMark = 95;
+// const heightMark = 1.88;
+// const massJohn = 85;
+// const heightJohn = 1.76;
+
+// const BMIMark = massMark / heightMark ** 2;
+// const BMIJohn = massJohn / (heightJohn * heightJohn);
+
+
+// console.log(BMIMark, BMIJohn);
+
+// if (BMIMark > BMIJohn) {
+//     console.log("Mark's BMI is higher than John's!")
+// } else {
+//     console.log("JOhns  BMI is higher than marky bois!")
+// }
+
+// const massMark = 78;
+// const heightMark = 1.69;
+// const massJohn = 92;
+// const heightJohn = 1.95;
+
+// const BMIMark = massMark / heightMark ** 2;
+// const BMIJohn = massJohn / (heightJohn * heightJohn);
+// console.log(BMIMark, BMIJohn);
+
+// if(BMIMark > BMIJohn) {
+//     console.log(`Marks BMI (${BMIMark}) IS WAY HIGHER THAN JOHNNYS(${BMIJohn})!`)
+// } else {
+//     console.log(`Johns BMI (${BMIJohn}) IS WAY HIGHER THAN MARKY BOIS!(${BMIMark})`)
+// }
+
+//type conversion
+// const inputYear = '1991';
+// console.log(Number(inputYear), inputYear);
+// console.log(Number(inputYear) + 18);
+
+// console.log(String(23), 23);
+
+// //type coercion + operator turns all numbers into strings
+// console.log('I am ' + 23 + ' years old');
+// console.log('23' - '10' - 3);
+// console.log('23' / '2');
+// console.log('23' > '18');
+
+// let n ='1' + 1;
+// n = n - 1;
+// console.log(n);
+// //answer is 10
+
+// 5 falsy values: 0, '', undefined, null, NaN
+
+// console.log(Boolean(0));
+// console.log(Boolean(undefined));
+// console.log(Boolean('Jonas'));
+// console.log(Boolean(''));
+
+// const money = 100;
+// if(money) {
+//     console.log("Don't Spend it all");
+// } else {
+//     console.log('You should get a job');
+// }
+
+// let height = 0;
+// if(height) {
+//     console.log('Yay! height is defined');
+// } else {
+//     console.log('Height is undefined');
+// }
+
+const age = 18;
+if(age === 18) console.log('You just became an adult');
+
+const favorite = Number (prompt("What's your favorite number?"));
+console.log(favorite);
+console.log(typeof favorite);
+
+if (favorite === 23) {
+    console.log('Cool! 23 is an amazing number!');
+} else if (favorite === 7) {
+    console.log('7 is also a cool number');
+} else if (favorite === 9) {
+    console.log('9 is also a sick number');
+} else {
+    console.log('Number is not 23 or 7');
+}
+
+if(favorite !== 23) console.log('Why not 23?');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
