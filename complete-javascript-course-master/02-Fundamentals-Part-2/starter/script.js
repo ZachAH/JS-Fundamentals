6ca4e83d0d1030fa47ff001b424d1887ad017917
ZMAN <etta1055@gmail.com>
2021-05-14 12:10:08 -0500

    arrow functions

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -36,17 +36,33 @@
 // console.log(water);
 
 //function declaration
-// function calcAge1 (birthYear) {
-//     return 2037- birthYear;
-// }
+function calcAge1 (birthYear) {
+    return 2037- birthYear;
+}
 
-// const age1 = calcAge1(1991);
-// console.log(age1);
+const age1 = calcAge1(1991);
+console.log(age1);
 
 // //function expression
-// const calcAge2 = function (birthYear) {
-//     return 2037- birthYear;
-// }
+const calcAge2 = function (birthYear) {
+    return 2037- birthYear;
+}
 
-// const age2 = calcAge2(1991);
-// console.log(age1, age2);
+const age2 = calcAge2(1991);
+console.log(age1, age2);
+
+// arrow function
+const calcAge3 = birthYear => 2037-birthYear;
+const age3 = calcAge3(1991);
+console.log(age3);
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+    const age = 2037 - birthYear;
+    const retirement = 65 - age;
+    //return retirement
+    return `${firstName} retires in ${retirement}`;
+}
+
+console.log(yearsUntilRetirement(1991, 'Zach'));
+console.log(yearsUntilRetirement(1980, 'Bob'));
+
