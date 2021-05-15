16e2ea569ec24a38217ab71d7eb1914108f4e926
ZMAN <etta1055@gmail.com>
2021-05-15 10:23:16 -0500

    calling functions inside functions

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -35,34 +35,73 @@
 // const water = fruitProcessor(0, 0);
 // console.log(water);
 
-//function declaration
-function calcAge1 (birthYear) {
-    return 2037- birthYear;
+// //function declaration
+// function calcAge1 (birthYear) {
+//     return 2037- birthYear;
+// }
+
+// const age1 = calcAge1(1991);
+// console.log(age1);
+
+// // //function expression
+// const calcAge2 = function (birthYear) {
+//     return 2037- birthYear;
+// }
+
+// const age2 = calcAge2(1991);
+// console.log(age1, age2);
+
+// // arrow function
+// const calcAge3 = birthYear => 2037-birthYear;
+// const age3 = calcAge3(1991);
+// console.log(age3);
+
+// const yearsUntilRetirement = (birthYear, firstName) => {
+//     const age = 2037 - birthYear;
+//     const retirement = 65 - age;
+//     //return retirement
+//     return `${firstName} retires in ${retirement}`;
+// }
+
+// console.log(yearsUntilRetirement(1991, 'Zach'));
+// console.log(yearsUntilRetirement(1980, 'Bob'));
+
+// function cutFruitPieces (fruit) {
+//     return fruit * 4;
+// }
+
+// function fruitProcessor(apples, oranges) {
+//     const applePieces = cutFruitPieces(apples);
+//     const orangePieces = cutFruitPieces(oranges);
+
+//     const juice = `Juice with ${applePieces} apples and ${orangePieces} oranges.`;
+//      return juice;
+// }
+
+// console.log(fruitProcessor(2, 3));
+
+const calcAge = function(birthYear) {
+    return 2037 - birthYear;
 }
 
-const age1 = calcAge1(1991);
-console.log(age1);
-
-// //function expression
-const calcAge2 = function (birthYear) {
-    return 2037- birthYear;
-}
-
-const age2 = calcAge2(1991);
-console.log(age1, age2);
-
-// arrow function
-const calcAge3 = birthYear => 2037-birthYear;
-const age3 = calcAge3(1991);
-console.log(age3);
-
-const yearsUntilRetirement = (birthYear, firstName) => {
-    const age = 2037 - birthYear;
+const yearsUntilRetirement = function (birthYear, firstName) {
+    const age = calcAge(birthYear);
     const retirement = 65 - age;
-    //return retirement
-    return `${firstName} retires in ${retirement}`;
+    
+    if(retirement > 0) {
+        //return ends function anything below will be ignored
+        return retirement
+        //this will be ignored 
+        // return `${firstName} retires in ${retirement} years`;
+    } else {
+        return -1;
+    }
+
+
+   
+    // return `${firstName} retires in ${retirement} years`;
 }
 
 console.log(yearsUntilRetirement(1991, 'Zach'));
-console.log(yearsUntilRetirement(1980, 'Bob'));
+console.log(yearsUntilRetirement(1970, 'Mike'));
 
