c552cd8ce257fe501e21f201cca3413121f5b6c1
ZMAN <etta1055@gmail.com>
2021-05-18 11:40:12 -0500

    added array methods

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -132,28 +132,78 @@
 // Apply this to the team's average scores 
 
 
-   const calcAverage = (a, b, c) => (a + b + c) /3;
-// console.log(calcAverage(3,4,5));
+//    const calcAverage = (a, b, c) => (a + b + c) /3;
+// // console.log(calcAverage(3,4,5));
 
-// use let instead of const so can change scores later
-let scoreDolphins = calcAverage(44,23,71);
-let scoreKoalas = calcAverage(65,54,49);
-console.log(scoreDolphins, scoreKoalas);
+// // use let instead of const so can change scores later
+// let scoreDolphins = calcAverage(44,23,71);
+// let scoreKoalas = calcAverage(65,54,49);
+// console.log(scoreDolphins, scoreKoalas);
 
-const checkWinner = function (avgDolphins, avgKoalas) {
-    if (avgDolphins >= 2* avgKoalas) {
-        console.log(`Dolphins Win(${avgDolphins}) vs. ${avgKoalas})`);
-    } else if (avgKoalas >= 2* avgDolphins) {
-        console.log(`Koalas Win(${avgKoalas}) vs. ${avgDolphins})`);
-    } else {
-        console.log('No team wins....');
-    }
-}
-checkWinner(scoreDolphins, scoreKoalas);
+// const checkWinner = function (avgDolphins, avgKoalas) {
+//     if (avgDolphins >= 2* avgKoalas) {
+//         console.log(`Dolphins Win(${avgDolphins}) vs. ${avgKoalas})`);
+//     } else if (avgKoalas >= 2* avgDolphins) {
+//         console.log(`Koalas Win(${avgKoalas}) vs. ${avgDolphins})`);
+//     } else {
+//         console.log('No team wins....');
+//     }
+// }
+// checkWinner(scoreDolphins, scoreKoalas);
 
-//test 2
-scoreDolphins = calcAverage(85,54,41);
-scoreKoalas = calcAverage(23,34,27);
-console.log(scoreDolphins, scoreKoalas);
-checkWinner(scoreDolphins, scoreKoalas);
+// //test 2
+// scoreDolphins = calcAverage(85,54,41);
+// scoreKoalas = calcAverage(23,34,27);
+// console.log(scoreDolphins, scoreKoalas);
+// checkWinner(scoreDolphins, scoreKoalas);
+
+// const friend1 = 'Mcheal';
+// const friend2 = 'Steven';
+// const friend3 = 'Peter';
+
+// const friends = ['Micheal', 'Steven', 'Peter'];
+// console.log(friends);
+// const y = new Array(1991, 1984, 2008, 2020);
+// //arrays start at index 0
+// console.log(friends[0]);
+// console.log(friends[2]);
+// //gets length of array
+// console.log(friends.length);
+// //gets the last element from array
+// console.log(friends[friends.length - 1]);
+
+// //mutates element at number 2 from Peter to Jay
+// friends[2] = 'Jay';
+// console.log(friends);
+//arrays are not primative values so you can mutate them even though they were declared with const
+
+//you can put an array inside another array
+// const zach = ['Zach', 'Howell', 2037-1991, 'student' , friends];
+// console.log(zach);
+// console.log(zach.length);
+
+
+// //Exerscise
+// const calcAge = function(birthYear) {
+//     return 2037 - birthYear;
+// }
+// const years = [1990, 1967, 2002, 2018];
+
+// const age1 = calcAge(years[0]);
+// const age2 = calcAge(years[1]);
+// const age3 = calcAge(years[years.length - 1]);
+// console.log(age1, age2, age3);
+
+// const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[years.length - 1])];
+// console.log(ages);
+
+
+const friends = ['Micheal', 'Steven', 'Peter'];
+//adds Jay to end of friends array
+const newLength = friends.push('Jay');
+console.log(friends);
+console.log(newLength);
+
+
+
 
