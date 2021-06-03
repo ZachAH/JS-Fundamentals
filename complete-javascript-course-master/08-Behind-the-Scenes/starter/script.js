268780e4207fb67fa5e5e2bfaa40d4dc3e0a0efc
ZMAN <etta1055@gmail.com>
2021-06-03 11:40:09 -0500

    arrow 2

--- complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
+++ complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
@@ -8,8 +8,14 @@ const jonas = {
     firstName: 'Jonas',
     year: 1991,
     calcAge: function () {
-        console.log(this);
+        //console.log(this);
         console.log(2037- this.year);
+
+        const self = this; // self or thatgit
+        const isMillenial = function() {
+            console.log(self.year >= 1981 && self.year <= 1996);
+        };
+        isMillenial();
     },
 
     greet: () => {
@@ -18,3 +24,4 @@ const jonas = {
     },
 };
 jonas.greet();
+jonas.calcAge();
