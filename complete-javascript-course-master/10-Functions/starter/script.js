a90f1c6a267a8db4a9526f6248163d7ad5902754
ZMAN <etta1055@gmail.com>
2021-06-25 12:32:26 -0500

    es6 defualt paremeters and functions

--- complete-javascript-course-master/10-Functions/starter/script.js
+++ complete-javascript-course-master/10-Functions/starter/script.js
@@ -1,1 +1,25 @@
 'use strict';
+
+const bookings = [];
+
+const createBooking = function (flightNum, numPassengers = 1, price = 199 * numPassengers) {
+ //ES5
+ //   numPassengers = numPassengers || 1;
+//   price = price || 199;
+
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+
+//second argument will always be mapped to second paremeter
+//to skip a default parameter and leave default type undefined
+createBooking('LH123');
+createBooking('LH123', 2, 800);
+createBooking('LH123', 2);
+createBooking('LH123', 5);
+createBooking('LH123', undefined, 1000);
